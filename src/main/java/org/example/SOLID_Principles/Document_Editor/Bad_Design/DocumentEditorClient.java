3b38ac4a02d8a564f02a0fb2bc6e311c818ca437
Ritwik <ritwikhero@gmail.com>
2026-06-27 14:15:34 +0530

    bad design done for document editor

--- src/main/java/org/example/SOLID_Principles/Document_Editor/Bad_Design/DocumentEditorClient.java
+++ src/main/java/org/example/SOLID_Principles/Document_Editor/Bad_Design/DocumentEditorClient.java
@@ -1,13 +1,14 @@
 package org.example.SOLID_Principles.Document_Editor.Bad_Design;
 
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DocumentEditorClient {
+class DocumentEditor {
     private List<String> documentElements;
     private String renderedDocument;
 
-    public DocumentEditorClient(){
+    public DocumentEditor(){
         documentElements = new ArrayList<>();
         renderedDocument = "";
     }
@@ -26,7 +27,41 @@ public class DocumentEditorClient {
     //render the current documents in editor
     public String renderDocument(){
         if(renderedDocument.isEmpty()){
-
+            StringBuilder result = new StringBuilder();
+            for(String element  : documentElements){
+                if(element.length() > 4 && (element.endsWith(".jpg") || element.endsWith(".png"))){
+                    result.append("[Image ").append(element).append("]\n");
+                }
+                else{
+                    result.append(element).append("\n");
+                }
+            }
+            renderedDocument = result.toString();
         }
+        return renderedDocument;
+    }
+
+    public void saveToFile(){
+         try{
+             FileWriter writer = new FileWriter("document.txt");
+             writer.write(renderDocument());
+             writer.close();
+             System.out.println("Document saved to document.txt");
+         }
+         catch (Exception e){
+             System.out.println("Error : unable to open file for writing");
+         }
+    }
+}
+
+public class DocumentEditorClient{
+    public static void main(String[] args) {
+        DocumentEditor editor = new DocumentEditor();
+        editor.addText("Added text to document");
+        editor.addImage("Image.png");
+        editor.addText("This is document editor");
+
+        System.out.println(editor.renderDocument());
+        editor.saveToFile();
     }
 }
